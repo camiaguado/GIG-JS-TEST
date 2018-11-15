5167c02f5de7804c03dee95368e3a92b3b5be7b1
camiaguado <camiaguado@hotmail.com>
2018-11-15 17:04:35 -0300

    Validations and forms

--- js/main.js
+++ js/main.js
@@ -46,6 +46,47 @@ var addressApp = angular.module('addressApp', ['ngRoute'])
 
 // Form:
 
-.controller('contactController', function($scope){
-	document.getElementsByTagName("html")[0].className = "contactController";	
+.controller('contactController', function($scope, $routeParams, $location){
+	document.getElementsByTagName("html")[0].className = "contactController";
+
+	$scope.goList = function(){
+		$location.path("/");
+	}
+	$scope.countries = window.countries;
+	$scope.new = true;
+	var id_param = Number($routeParams.id);	
+	if(!_.isNaN(id_param)){
+		$scope.savedContact = _.find($scope.contacts, function(obj){
+			return obj.id == id_param;
+		});
+		$scope.contact = angular.copy($scope.savedContact);
+		$scope.new = false;
+	}
+	$scope.add = function(){
+		var new_contact = new Contact();
+		_.forIn($scope.contact, function(value, key) {
+			new_contact[key] = value;
+		});
+		$scope.contacts.push(new_contact);
+		returnHome();
+	}
+	$scope.update = function(){
+		_.forIn($scope.contact, function(value, key) {
+			$scope.savedContact[key] = value;
+		});
+		returnHome();
+	}
+	$scope.delete = function(){
+		var deleted = _.remove($scope.contacts, function(object){
+			return object.id == $scope.contact.id;
+		});
+		returnHome();
+	}
+	var returnHome = function(){
+		persistData();
+		$scope.goList();
+	}
+	var persistData =  function() {
+		localStorage.setItem("addressApp_data", JSON.stringify($scope.contacts));
+	}
 })
